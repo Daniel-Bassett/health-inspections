1eaa81bf29ead29d2459598184d49c96bfa1eb6c
Daniel B <daniel.m.bassett@outlook.com>
2023-01-30 15:40:25 -0600

    changed colors and moved around the layout

--- app.py
+++ app.py
@@ -11,9 +11,16 @@ df['name & address'] = df['Restaurant Name'] + ' ' + df['Address']
 customdata = np.stack((df['Restaurant Name'], df['Score'], df['Address'], df['Inspection Date']),  axis=-1)
 
 # st.dataframe(df)
-st.title("Austin Restaurants - Inspection Score")
+st.title("Austin Restaurants Inspection Score")
 
-plot_spot = st.empty()
+col1, col2 = st.columns([1, 1])
+with col1:
+    selection = st.selectbox('Choose Restaurant', options=df['name & address'], label_visibility='visible')
+with col2:
+    df_plot = st.empty()
+button_plot = st.empty()
+graph_plot = st.empty()
+
 
 fig = px.scatter_mapbox(df,
                         lon=df['new_longitude'],
@@ -21,23 +28,22 @@ fig = px.scatter_mapbox(df,
                         zoom=10,
                         color=df['Score'],
                         text=df['Restaurant Name'],
-                        color_continuous_scale=['red', 'green'],
-                        hover_data=['Restaurant Name']
+                        color_continuous_scale='RdYlBu',
+                        hover_data=['Restaurant Name'],
+                        range_color=[65, 100],
 )
 fig.update_layout(mapbox_style='open-street-map')
 fig.update_layout(showlegend=False)
 fig.update_traces(customdata=customdata, hovertemplate='Name: %{customdata[0]}<br>Score: %{customdata[1]}</br>Address: %{customdata[2]}<br>Inspection Date: %{customdata[3]}</br>')
 
-
-selection = st.selectbox('Choose Restaurant', options=df['name & address'])
-
-st.dataframe(df[df['name & address'] == selection][['Restaurant Name', 'Score', 'Inspection Date']])
+with df_plot:
+    st.dataframe(df[df['name & address'] == selection][['Restaurant Name', 'Score', 'Inspection Date']])
 latt=df[df['name & address'] == selection]['new_latitude'].values[0]
 lonn=df[df['name & address'] == selection]['new_longitude'].values[0]
 
+with button_plot:
+    if st.button("Center Map"):
+        fig.update_layout(mapbox_center=dict(lat=latt, lon=lonn), mapbox_zoom=17)
 
-if st.button("Center Map"):
-    fig.update_layout(mapbox_center=dict(lat=latt, lon=lonn), mapbox_zoom=17)
-
-with plot_spot:
+with graph_plot:
     st.plotly_chart(fig)
