a381f94d67150b9b427c46b6d4a4e5b485840d3f
Daniel B <daniel.m.bassett@outlook.com>
2023-01-30 23:06:50 -0600

    started work on filtering by score

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import streamlit as st
+from streamlit_option_menu import option_menu
 
 
 df = pd.read_csv('data/inspection-scores.csv')
@@ -10,16 +11,25 @@ df.reset_index(drop=True, inplace=True)
 df['name & address'] = df['Restaurant Name'] + ' ' + df['Address']
 customdata = np.stack((df['Restaurant Name'], df['Score'], df['Address'], df['Inspection Date']),  axis=-1)
 
-# st.dataframe(df)
-st.title("Austin Restaurants Inspection Score")
 
-col1, col2 = st.columns([1, 1])
-with col1:
-    selection = st.selectbox('Choose Restaurant', options=df['name & address'], label_visibility='visible')
-with col2:
-    df_plot = st.empty()
-button_plot = st.empty()
-graph_plot = st.empty()
+with st.sidebar:
+    selected = option_menu(
+        menu_title='Main Menu',
+        options=['Inspection Scores', 'Filtered Scores']
+    )
+if selected == 'Inspection Scores':
+    st.title("Austin Restaurant Inspection Scores")
+
+    col1, col2 = st.columns([1, 1])
+    with col1:
+        selection = st.selectbox('Choose Restaurant', options=df['name & address'], label_visibility='visible')
+    with col2:
+        df_plot = st.empty()
+    button_plot = st.empty()
+    graph_plot = st.empty()
+if selected == 'Filtered Scores':
+    st.slider('Score Range', value=[30, 100])
+    pass
 
 
 fig = px.scatter_mapbox(df,
@@ -28,7 +38,7 @@ fig = px.scatter_mapbox(df,
                         zoom=10,
                         color=df['Score'],
                         text=df['Restaurant Name'],
-                        color_continuous_scale='RdYlBu',
+                        color_continuous_scale=['Red', 'Blue'],
                         hover_data=['Restaurant Name'],
                         range_color=[65, 100],
 )
@@ -42,8 +52,8 @@ latt=df[df['name & address'] == selection]['new_latitude'].values[0]
 lonn=df[df['name & address'] == selection]['new_longitude'].values[0]
 
 with button_plot:
-    if st.button("Center Map"):
-        fig.update_layout(mapbox_center=dict(lat=latt, lon=lonn), mapbox_zoom=17)
+    if st.button("Go To Restaurant"):
+        fig.update_layout(mapbox_center=dict(lat=latt, lon=lonn), mapbox_zoom=16)
 
 with graph_plot:
     st.plotly_chart(fig)
